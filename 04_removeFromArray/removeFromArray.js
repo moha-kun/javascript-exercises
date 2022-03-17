d340526685594b93732bb704a63c89acd2874a4b
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-03-17 13:38:49 +0100

    ansewring the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...restParameter) {
 
+    for (let i = 0; i < restParameter.length; i++)
+        for (let j = 0; j < array.length; j++)
+            if (array[j] === restParameter[i])
+                array.splice(j, 1);
+
+    return array;
 };
 
 // Do not edit below this line
