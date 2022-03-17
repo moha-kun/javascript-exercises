ab8a6ff66e33baec3bcab519c476768ec8855701
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-03-17 13:38:23 +0100

    ansewring the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    return num < 0 ? "ERROR" :
+        num === 0 ? "" :
+            str + repeatString(str, num - 1);
 };
 
 // Do not edit below this line
