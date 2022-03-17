79d05e6873dbe1855da99bdf51f676385bee0370
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-03-18 00:05:01 +0100

    answering the sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if ((!(year % 4) && year % 100) || !(year % 400)) return true;
+    return false;
 };
 
 // Do not edit below this line
