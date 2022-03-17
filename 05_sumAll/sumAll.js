fa0ab938c044c35e5903c24f937b197a2ac26a2f
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-03-18 00:04:26 +0100

    answering the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (typeof (num1) !== "number" || typeof (num2) !== "number") return "ERROR";
+    if (num1 * num2 < 0) return "ERROR";
+    if (num1 < num2) return num1 == num2 ? num1 : num1 + sumAll(num1 + 1, num2);
+    return num1 == num2 ? num1 : num1 + sumAll(num1 - 1, num2);
 };
 
 // Do not edit below this line
