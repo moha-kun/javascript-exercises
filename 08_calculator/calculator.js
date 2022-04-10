939c2eb2f81ef2809c1bcef51ffe7175771dd6b9
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-04-10 12:44:40 +0000

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  let args = Array.from(arguments[0]);
+  args.unshift(0);
+
+  for (let i = 0; i < args.length; i++) {
+    args[0] += args[i];
+  }
+
+  return args[0];
 };
 
-const multiply = function() {
+const multiply = function () {
+  let args = Array.from(arguments[0]);
+  args.unshift(1);
 
+  for (let i = 0; i < args.length; i++) {
+    args[0] *= args[i];
+  }
+
+  return args[0];
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (args) {
+  return args ? args * factorial(args - 1) : 1;
 };
 
 // Do not edit below this line
