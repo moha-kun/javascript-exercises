fc2ef70a5a1fb68660b5946176e24c0e01ea96c7
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-04-10 12:44:49 +0000

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z]/g, "");
+    return string.includes(string.split("").reverse().join(""));
 };
 
 // Do not edit below this line
