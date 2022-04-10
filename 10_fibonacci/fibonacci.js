842fcfe9822226e7ca4f1866b03b54603119ddb5
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-04-10 12:44:57 +0000

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let arr = [1, 1, 0];
+    for (let i = 0; i < num - 2; i++) {
+        arr[2] = arr[1] + arr[0];
+        arr[0] = arr[1];
+        arr[1] = arr[2];
+    }
+    return arr[1];
 
 };
 
