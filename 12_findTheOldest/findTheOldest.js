6b043fb71fff7ce64053845aa9b555af96b602c3
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-04-10 12:45:09 +0000

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((currentPerson, nextPerson) => {
+        if (!currentPerson.yearOfDeath) currentPerson.yearOfDeath = new Date().getFullYear();
+        if (!nextPerson.yearOfDeath) nextPerson.yearOfDeath = new Date().getFullYear();
+        return (currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (nextPerson.yearOfDeath - nextPerson.yearOfBirth) ? currentPerson : nextPerson;
+    }, people[0]);
 };
 
 // Do not edit below this line
